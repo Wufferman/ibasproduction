6a6b86ef4e977d845c60ea27defb4b08ebb84d17
Henrik L. Jensen <hnrk@eaaa.dk>
2023-02-14 17:29:10 +0100

    First commit

---
+++ Models/DailyProductionDTO.cs
@@ -0,0 +1,19 @@
+namespace DailyProduction.Models;
+
+public enum BikeModel {
+    undefined = 0,
+    IBv1 = 1,
+    evIB100 = 2,
+    evIB200 = 3
+}
+
+/// <summary>
+/// An container entity for a single daily production of a bike model.
+/// </summary>
+public class DailyProductionDTO
+{
+    public DateTime Date { get; set; }
+    public BikeModel Model { get; set; }
+    public int ItemsProduced { get; set; }
+
+}
